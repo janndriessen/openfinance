5d5b2f3af961dc8f6186041dbeec496f9b5cd9b0
JD <jann.driessen@gmx.de>
2024-07-12 21:28:29 +0200

    add empty app

---
+++ app/openfinance/openfinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  openfinanceApp.swift
+//  openfinance
+//
+//  Created by Jann Driessen on 12.07.24.
+//
+
+import SwiftUI
+
+@main
+struct openfinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
