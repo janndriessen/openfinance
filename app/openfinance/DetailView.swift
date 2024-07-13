6eaceb34831436d56bd978fe954172729603fa31
JD <jann.driessen@gmx.de>
2024-07-13 08:01:27 +0200

    add line chart

--- app/openfinance/DetailView.swift
+++ app/openfinance/DetailView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct DetailView: View {
+    let data = [
+        DataPoint(x: 0, y: 1),
+        DataPoint(x: 1, y: 3),
+        DataPoint(x: 2, y: 2),
+        DataPoint(x: 3, y: 4),
+        DataPoint(x: 4, y: 3)
+    ]
     var body: some View {
         ZStack {
             OFColor.background.edgesIgnoringSafeArea(.all)
-            Text("AAPL")
-                .font(.title)
-                .foregroundStyle(OFColor.black)
+            VStack {
+                Text("AAPL")
+                    .font(.title)
+                    .foregroundStyle(OFColor.black)
+                LineChartView(data: data)
+            }
         }
     }
 }
---
+++ app/openfinance/components/LineChartView.swift
@@ -0,0 +1,46 @@
+//
+//  LineChartView.swift
+//  openfinance
+//
+//  Created by Jann Driessen on 13.07.24.
+//
+
+import Charts
+import SwiftUI
+
+struct DataPoint: Identifiable {
+    let id = UUID()
+    let x: Double
+    let y: Double
+}
+
+struct LineChartView: View {
+    var data: [DataPoint]
+
+    var body: some View {
+        Chart(data) { point in
+            LineMark(
+                x: .value("X Value", point.x),
+                y: .value("Y Value", point.y)
+            )
+            .foregroundStyle(OFColor.black)
+        }
+        .chartXAxis(.hidden)
+        .chartYAxis(.hidden)
+        .chartPlotStyle { plotArea in
+            plotArea.background(.clear)
+        }
+        .frame(height: 200)
+    }
+}
+
+#Preview {
+    let data = [
+        DataPoint(x: 0, y: 1),
+        DataPoint(x: 1, y: 3),
+        DataPoint(x: 2, y: 2),
+        DataPoint(x: 3, y: 4),
+        DataPoint(x: 4, y: 3)
+    ]
+    return LineChartView(data: data)
+}
